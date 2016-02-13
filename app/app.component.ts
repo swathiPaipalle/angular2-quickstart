7a80778ee2edd7e315157401ec35f6b2371b7224
Swathi Paipalle <swathi.paipalle@gmail.com>
2016-02-12 16:15:30 -0800

    Adding files

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/hello.html'
+    //template:"<h1>My Angular 2 App</h1><b style='color:dodgerblue'>Hello </b><h3 style='color:red; display: inline;':>Angular 2</h3>"
+})
+export class AppComponent { }
